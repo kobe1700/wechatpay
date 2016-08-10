599a393ba41ca53fc9bef5d32a2d77c09f4189c1
haogre <haogre@gmail.com>
2016-08-10 11:31:33 +0800

    add testController add spring-web deps

---
+++ src/main/java/com/haogre/wechat/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.haogre.wechat.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/**
+ * @author HAOZ
+ * @date 2016-8-10
+ * @description TestController4Test
+ *
+ */
+@RequestMapping(value="/test")
+@Controller
+public class TestController {
+
+}
